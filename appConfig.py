c6b535beb7ea298472b187f8093f0355fae13c55
Gabriel PC casa <gsrfilho9@gmail.com>
2025-05-08 23:42:53 -0300

    Interface do aplicativo feita, agora criar funções

--- appConfig.py
+++ appConfig.py
@@ -1,5 +1,7 @@
 import tkinter as tk
 from tkinter import ttk
+import matplotlib.pyplot as plt
+from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg, NavigationToolbar2Tk
 
 class App (tk.Tk):
     def __init__(self):
@@ -20,30 +22,90 @@ class FrameTop(ttk.Frame):
     def __init__(self, parent):
         super().__init__(parent)
         
-    def creat_widget(self):
-        ...
-        #Criar o widget para plotar o gráfico
+        self.creat_widgets()
+        self.creat_layout()
+        
+    def creat_widgets(self):
+        self.fig, self.ag = plt.subplots()
+        self.canvas = FigureCanvasTkAgg(self.fig, master=self)
+        self.canvas.get_tk_widget().pack(side=tk.TOP, fill=tk.BOTH, expand=1)
+    
+    def creat_layout(self):
+        self.place(relx=0.02, rely=0.02, relwidth=0.96, relheight=0.39)
+        
         
 class FrameMid(ttk.Frame):
     def __init__(self, parent):
         super().__init__(parent)
         
-    def creat_widget(self):
-        potMaLabel = tk.Label(self, text = 'Pma =')
-        potMbLabel = tk.Label(self, text = 'Pmb =')
-        potMcLabel = tk.Label(self, text = 'Pmc =')
+        self.creat_widgets()
+        self.creat_layout() 
         
-        potMaEntry = tk.Entry(self)
-        potMbEntry = tk.Entry(self)
-        potMcEntry = tk.Entry(self)
+    def creat_widgets(self):
+        self.potMaLabel = tk.Label(self, text = 'Pma =')
+        self.potMbLabel = tk.Label(self, text = 'Pmb =')
+        self.potMcLabel = tk.Label(self, text = 'Pmc =')
+        
+        self.potMaEntry = tk.Entry(self)
+        self.potMbEntry = tk.Entry(self)
+        self.potMcEntry = tk.Entry(self)
+        
+    def creat_layout(self):
+        self.place(relx=0, rely=0.43, relwidth=1, relheight=0.03)
+        
+        self.potMaLabel.place(relx=0, rely=0, relwidth=0.1, relheight=1)
+        self.potMbLabel.place(relx=0.15, rely=0, relwidth=0.1, relheight=1)
+        self.potMcLabel.place(relx=0.3, rely=0, relwidth=0.1, relheight=1)
+        
+        self.potMaEntry.place(relx=0.07, rely=0, relwidth=0.1, relheight=1)
+        self.potMbEntry.place(relx=0.22, rely=0, relwidth=0.1, relheight=1)
+        self.potMcEntry.place(relx=0.37, rely=0, relwidth=0.1, relheight=1)
         
 class FrameBot(ttk.Frame):
     def __init__(self, parent):
         super().__init__(parent)
         
+        self.creat_widgets()
+        self.creat_layout()
+        
     def creat_widgets(self):
-        botaoRodar = ttk.Button(self, text = 'Rodar')
-        botaoPlot = ttk.Button(self, text = 'Plotar Curva')
-        #===========
-        #Criar o lugar para mostrar os resultados, pensar ainda como serão exibidos
-        #===========
+        #Subframes:
+        self.frameResults = ttk.Frame(self)
+        self.frameGraficoDeseq = ttk.Frame(self)
+        self.frameBotoes = ttk.Frame(self)
+        
+        #Botoes:
+        self.botaoRodar = ttk.Button(self.frameBotoes, text = 'Rodar')
+        self.botaoPlot = ttk.Button(self.frameBotoes, text = 'Curva de Carga')
+        
+        #Gráfico:
+        self.fig, self.ag = plt.subplots()
+        self.canvas = FigureCanvasTkAgg(self.fig, master=self.frameGraficoDeseq)
+        self.toolbar = NavigationToolbar2Tk(self.canvas, self.frameGraficoDeseq)
+        self.toolbar.update()
+        
+        #Treeview:
+        self.tree = ttk.Treeview(self.frameResults)
+        self.treescrolly = ttk.Scrollbar(self.frameResults, orient="vertical", command=self.tree.yview)
+        self.treescrollx = ttk.Scrollbar(self.frameResults, orient="horizontal", command=self.tree.xview)
+        self.tree.configure(yscrollcommand=self.treescrolly.set, xscrollcommand=self.treescrollx.set)
+        
+        
+    def creat_layout(self):
+        self.place(relx=0.02, rely=0.48, relwidth=0.96, relheight=0.5)
+        
+        self.frameResults.place(relx=0, rely=0, relwidth=0.5, relheight=0.92)
+        self.frameGraficoDeseq.place(relx=0.5, rely=0, relwidth=0.5, relheight=0.92)
+        self.frameBotoes.place(relx=0, rely=0.92, relwidth=1, relheight=0.08)
+        
+        self.botaoRodar.place(relx=0.8, rely=0, relwidth=0.1, relheight=1)
+        self.botaoPlot.place(relx=0.9, rely=0, relwidth=0.1, relheight=1)
+        
+        self.canvas.get_tk_widget().place(relx=0, rely=0, relwidth=1, relheight=0.9)
+        self.toolbar.place(relx=0, rely=0.9, relwidth=1, relheight=0.1)
+        
+        self.tree.place(relx=0, rely=0, relwidth=1, relheight=1)
+        self.treescrolly.pack(side="right", fill='y')
+        self.treescrollx.pack(side="bottom", fill='x')
+        
+App()
